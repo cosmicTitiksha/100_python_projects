cdadb4ee01636c1ccceaa3b21df4521c52bb3547
Titiksha <t02437356@gmail.com>
2025-10-25 15:27:34 +0530

    to do list with files

--- to_do_list/to_do_list_with_files.py
+++ to_do_list/to_do_list_with_files.py
@@ -1,63 +1,69 @@
-# This program contains To-Do list where list of tasks would be stored in a file for permanent storage and not delete with every sync
+# This program is To-Do list, where tasks are saved in a file for long term storage
+
 TASK_FILE = 'todo_list.txt'
 
+# loading tasks stored in a file into a list
 def load_tasks():
     try:
         with open(TASK_FILE, 'r') as f:
-            tasks = [line.strip() for line in f.readlines()]
+            tasks = [lines.strip() for lines in f.readlines()]
         return tasks
     except FileNotFoundError:
         return []
-    
+
+# Create task
+def create_task():
+    task = input("Enter the task, you want to add : ")
+    return task
+
+# Save task
 def save_tasks(tasks):
     with open(TASK_FILE, 'w') as f:
         for task in tasks:
             f.write(task + '\n')
-    print("Task saved successfully!")
+    print("Task saved successfully!!!!")
 
-def create_task():
-    task = input("Enter the task : ")
-    return task
-
-def view_task(tasks):
-    if not tasks:
-        print("\nYour To-Do list is empty.")
-        return
-    print("\n----------Current To Do List---------------")
+# Viewing tasks
+def view_tasks(tasks):
     for i, task in enumerate(tasks, start=1):
         print(f"{i}. {task}")
-    print("-----------------------------------------------------------")
 
-def delete_task(index, tasks):
+# Deleting tasks
+def delete_task(tasks, index):
     tasks.pop(index-1)
     return tasks
 
+# Program begins executing from here
 tasks = load_tasks()
+
+# Loop to keep the operation going...until exiting the loop
 while True:
-    query = input("What operation do you want to perform (add/delete/view/exit) tasks : ")
+    # Asking the user, which oeration to perform
+    query = input("What operation do you to perform, (add/view/delete/exit) tasks ? : ")
+    # logic if user entered 'add'
     if query.lower() == 'add':
         tasks.append(create_task())
         save_tasks(tasks)
     
+    # if user entered 'View'
     elif query.lower() == 'view':
-        view_task(tasks)
+        view_tasks(tasks)
 
+    # if user entered 'delete'
     elif query.lower() == 'delete':
-        view_task(tasks)
         try:
-            index = int(input("Enter the # of the task, you want to delete : "))
+            index = int(input("Enter the # you want to delete : "))
             if 1 <= index <= len(tasks):
-                delete_task(index, tasks)
+                tasks = delete_task(tasks, index)
                 save_tasks(tasks)
-            else:
-                print("Error: The number is not in the list")
         except ValueError:
-            print("Error: Please enter a valid number.")
-    
+            print("Please enter the valid literal")
+
+    # if user entered 'exit'
     elif query.lower() == 'exit':
+        print("✌️ Exiting.✌️")
         break
 
-    else:
-        print("Invalid operation...please enter a valid query.")
-
-print("Thanks for using the To-Do list. GoodBye!")
+# part, outside the loop
+print("Thanks for using To-Do list. GoodBye!!!!")
+    
